67db6bd29e53820eb89eca657cff98eafe2767d1
vpagano10 <vpagano10@gmail.com>
2019-11-01 12:04:43 -0400

    almost done with mvp some minor changes needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+// import axios from 'axios';
+import { data } from './data';
 import './App.css';
+import Navbar from './components/NavBar';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Div = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 2%;
+  font-size: 1.8rem;
+`;
+const Players = styled.div`
+  width: 20%;
+  margin: 2%;
+  margin-top: 2%;
+  padding: 1%;
+  border: 4px solid black;
+  border-radius: 5px;
+  line-height: 1.2;
+  background-color: slategrey;
+  &:hover {
+    background-color: grey;
+    border: 4px solid dodgerblue;
+  }
+`;
+const Name = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  color: khaki;
+`;
+const Country = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  color: lightblue;
+`;
+const Searches = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  color: firebrick;
+`;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []   
+    }
+  }
+
+  componentDidMount() {
+    // axios
+    //   .get('https://raw.githubusercontent.com/googletrends/data/master/20190815_WomensWorldCup.csv')
+    //   .then(res => {
+    //     console.log(res.data);
+    //   })
+    //   .catch(err => {
+    //     console.log('Data not collected', err)
+    //   })
+    this.setState({ players: data})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Div>
+          {this.state.players.map(player => (
+            <Players key={player.id}><Name>{player.name}</Name><br /><Country>Country: </Country> {player.country}<br /><Searches>{player.searches}</Searches></Players>
+          ))}
+        </Div>
+      </div>
+    );
+  }
 }
-
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('it displays Most Searched Players', () => {
+  const { getByText } = render(<App />);
+  getByText(/Most Searched Players/i)
+})
+
+test('renders without crashing', () => {
+  render(<App />);
 });
